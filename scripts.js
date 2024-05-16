75737a490118b739cdbc13603112756efcfeb4e2
RichardTheEngineer <150022242+RichardTheEngineer@users.noreply.github.com>
2024-05-16 15:35:22 +0100

    Add incremental opacity effect.

--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ const resetbutton = document.querySelector("#resetbtn");
 const eraserButton = document.querySelector("#eraserbtn");
 
 let gridColor = "black";
-let size = 2500;
+let size = 40**2;
+let height = (1/Math.sqrt(size))*100;
 
 for (let i=0; i<size; i++){
     let gridDiv = document.createElement("div");
     // gridDiv.textContent = i;
     gridDiv.setAttribute("class", "grid")
+    gridDiv.style.height = height + "%";
+    gridDiv.style.width = height + "%";
+    gridDiv.style.opacity = 0;
     container.appendChild(gridDiv);
 };
 
@@ -18,24 +22,32 @@ const gridDivs = document.querySelectorAll(".grid");
 gridDivs.forEach((gridDiv)=> {
     gridDiv.addEventListener("mouseover", (e)=> {
         e.target.style.backgroundColor = gridColor;
+        const currentOpacity = e.target.style.opacity;
+        const newOpacity = Number(currentOpacity) + 0.1;
+        e.target.style.opacity = newOpacity;
+
     })
 })
 
 function resetClick() {
     gridDivs.forEach((gridDiv)=> {
-        gridDiv.style.backgroundColor = "antiquewhite";
+        gridDiv.style.backgroundColor = "papayawhip";
+        gridColor = "black"
     })
     
 }
 
 function eraserClick() {
+
+    eraserButton.classList.toggle("active");
     if (gridColor == "black") {
-        gridColor = "antiquewhite";
+        gridColor = "papayawhip";
     }
     else {
         gridColor = "black";
     }
 }
+
 
 eraserButton.addEventListener("click", eraserClick);
 resetbutton.addEventListener("click", resetClick);
