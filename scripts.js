f107113a6aceef03b75ba385ebec3e4beb82014d
RichardTheEngineer <150022242+RichardTheEngineer@users.noreply.github.com>
2024-05-17 06:58:34 +0100

    Add button to change the side length of the grid
    code needs to be refactored

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const container = document.querySelector("#container");
 const para = document.createElement("p");
 const resetbutton = document.querySelector("#resetbtn");
-const eraserButton = document.querySelector("#eraserbtn");
+const eraserButton = document.querySelector("#eraserbtn")
+const sizeButton = document.querySelector("#sideLength")
 
 let gridColor = "black";
-let size = 40**2;
+let size = 4**2;
 let height = (1/Math.sqrt(size))*100;
+let inputSize = document.querySelector("#sideLength");
 
 for (let i=0; i<size; i++){
     let gridDiv = document.createElement("div");
@@ -17,7 +19,7 @@ for (let i=0; i<size; i++){
     container.appendChild(gridDiv);
 };
 
-const gridDivs = document.querySelectorAll(".grid");
+let gridDivs = document.querySelectorAll(".grid");
 
 gridDivs.forEach((gridDiv)=> {
     gridDiv.addEventListener("mouseover", (e)=> {
@@ -53,5 +55,36 @@ function eraserClick() {
 
 eraserButton.addEventListener("click", eraserClick);
 resetbutton.addEventListener("click", resetClick);
+sizeButton.addEventListener("click", () => {
+    input = (prompt("Type in the side length (Max: 100): ", "50" ))**2;
+    if (input <= 10000){
+        size = input;
+    }
+    else {
+        size = 100**2;
+    }
+    height = (1/Math.sqrt(size))*100;
+    container.innerHTML = ''
+    for (let i=0; i<size; i++){
+        let gridDiv = document.createElement("div");
+        // gridDiv.textContent = i;
+        gridDiv.setAttribute("class", "grid")
+        gridDiv.style.height = height + "%";
+        gridDiv.style.width = height + "%";
+        gridDiv.style.opacity = 0;
+        container.appendChild(gridDiv);
+    };
+    gridDivs = document.querySelectorAll(".grid");
+    gridDivs.forEach((gridDiv)=> {
+        gridDiv.addEventListener("mouseover", (e)=> {
+            e.target.style.backgroundColor = gridColor;
+            const currentOpacity = e.target.style.opacity;
+            const newOpacity = Number(currentOpacity) + 0.1;
+            e.target.style.opacity = newOpacity;
+    
+        })
+    })
+})
+
 
     
